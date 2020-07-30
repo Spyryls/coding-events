3f174e7c9c044f0b48f1a3eb31caabafb9dece7e
Spyryls <yr410ad@gmail.com>
2020-07-30 11:24:02 -0500

    Quick Save: Testing Appearance in GitHub

--- src/main/java/org/launchcode/codingevents/data/TagRepository.java
+++ src/main/java/org/launchcode/codingevents/data/TagRepository.java
@@ -2,6 +2,10 @@ package org.launchcode.codingevents.data;
 
 import org.launchcode.codingevents.models.Tag;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
 
 public interface TagRepository extends CrudRepository<Tag, Integer> {
 }
